713c826b704d8aacfb0731c027b64059b31b74f5
Matt Watson <matt@devpartners.io>
2021-06-04 15:49:56 -0400

    Make the page pretty

--- index.php
+++ index.php
@@ -2,3 +2,14 @@
 
 // That's it
 http_response_code(404);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="dist/tailwind.css">
+</head>
+<body class="flex flex-col justify-center h-screen">
+<h1 class="text-center text-9xl text-gray-300 font-bold">404</h1>
+</body>
+</html>
+
